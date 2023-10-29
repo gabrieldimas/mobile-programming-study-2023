e6c7434adc5a84561a4d854d1ea643b4f4922591
Gabriel Dimas <93063866+gabrieldimas@users.noreply.github.com>
2023-10-29 09:33:46 +0700

    update on app_asset

---
+++ UTS/uts/lib/config/app_asset.dart
@@ -0,0 +1,12 @@
+class AppAsset {
+  static const icon_linkaja = 'assets/images/icon_linkaja.png';
+  static const icon_voucher = 'assets/images/icon_voucher.png';
+  static const icon_fav = 'assets/images/icon_fav.png';
+  static const icon_red_arrow = 'assets/images/icon_red_arrow.png';
+
+  static const icon_home = 'assets/images/icon_home.png';
+  static const icon_history = 'assets/images/icon_history.png';
+  static const icon_pay = 'assets/images/icon_pay.png';
+  static const icon_inbox = 'assets/images/icon_inbox.png';
+  static const icon_account = 'assets/images/icon_account.png';
+}
